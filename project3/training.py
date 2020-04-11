a8e64f398ac819074ff7eba87ae1a71f126ed317
noxmoon <gheowyn@gmail.com>
2020-04-11 13:59:15 +0000

    pandas Q table

--- project3/training.py
+++ project3/training.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def epsilon_greedy(learner, env, s, epsilon):
     if np.random.rand()<epsilon:
@@ -9,22 +10,24 @@ def epsilon_greedy(learner, env, s, epsilon):
         return learner.select_action(s)
     
 def learning(learner, env, 
-             epsilon=1, epsilon_decay=0.9995, epsilon_min=0.1, 
-             alpha=1, alpha_decay=0.9995, alpha_min=0.001, 
+             epsilon=1, epsilon_decay=0.9999, epsilon_min=0.1, 
+             alpha=1, alpha_decay=0.9999, alpha_min=0.001, 
              gamma = 0.9,
-             max_episode=10000):
+             max_iteration=200000):
     
     np.random.seed(10)
     
     hist = []
-    for i in tqdm(range(max_episode)):
-        s = env.reset()
-        done = False
-        while not done:
-            action = epsilon_greedy(learner, env, s, epsilon)
-            next_s, r, done, info = env.step(action)
-            learner.update(s, next_s, action, r, done, alpha, gamma)
-            s = next_s
+    s = env.reset()
+    done = False
+    for i in tqdm(range(max_iteration)):
+        if done:
+            s = env.reset()
+        
+        action = epsilon_greedy(learner, env, s, epsilon)
+        next_s, r, done, info = env.step(action)
+        learner.update(s, next_s, action, r, done, alpha, gamma)
+        s = next_s
             
         epsilon = max(epsilon*epsilon_decay, epsilon_min)
         alpha = max(alpha*alpha_decay, alpha_min)
@@ -37,7 +40,7 @@ def learning(learner, env,
 def learning_2005(learner, env, 
              epsilon=1, 
              gamma = 0.9,
-             max_episode=10000):
+             max_iteration=200000):
 # following Greenwald 2005 paper LR decaying schedule: 
 # decay by 1/n(s,a) n(s,a) is # of visit to state-action pair
 
@@ -47,18 +50,20 @@ def learning_2005(learner, env,
     c = np.zeros([env.nS]+list(env.nA))
     
     hist = []
-    for i in tqdm(range(max_episode)):
-        s = env.reset()
-        done = False
-        while not done:
-            action = epsilon_greedy(learner, env, s, epsilon)
-            next_s, r, done, info = env.step(action)
-            learner.update(s, next_s, action, r, done, a[s][action], gamma)
+    s = env.reset()
+    done = False
+    for i in tqdm(range(max_iteration)):
+        if done:
+            s = env.reset()
+
+        action = epsilon_greedy(learner, env, s, epsilon)
+        next_s, r, done, info = env.step(action)
+        learner.update(s, next_s, action, r, done, a[s][action], gamma)
             
-            c[s][action] += 1
-            a[s][action] = 1/c[s][action]
+        c[s][action] += 1
+        a[s][action] = 1/c[s][action]
          
-            s = next_s
+        s = next_s
         
         hist.append(learner.log_value())
         
@@ -69,7 +74,15 @@ def plot_error(hist, f):
     plt.figure(figsize=(7,5))
     plt.plot(errors)
     plt.ylim(0,0.5)
-    plt.xlabel("episode")
+    plt.xlabel("iteration")
     plt.ylabel("Q value change")
     plt.savefig(f)
     plt.show()
+    
+    
+def show_table(Q):
+    a = ['stick', 'W', 'E', 'N', 'S']
+    c = pd.Index(a, name='player A')
+    r = pd.Index(a, name='player B')
+    table = pd.DataFrame(Q, index=c, columns=r).round(3)
+    display(table)
